9bf77fef073f4d73b153614d953884059dcbe889
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-17 05:06:25 +0300

    completing the user interface

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function registered_successifully(){
-    document.getElementsById('register-page').style.display = 'none';
+// A function to display the floating action buttons
+function actionButtons() {
+  let buttons = document.getElementById('floating-buttons-container');
+    if(buttons.style.display=== "none"){
+        buttons.style.display= "block";
+    }else {
+        buttons.style.display = "none";
+    }
+    let button_show = document.getElementById('add-file-button');
+   button_show.addEventListener("blur", () => {
+       buttons.style.display = "none";
+    });
 }

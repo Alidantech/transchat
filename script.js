397164c8c814aa730ef26fcb1bd0b664f424092e
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-18 00:54:38 +0300

    complete UI

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ function actionButtons() {
        buttons.style.display = "none";
     });
 }
+// function to open the chat page for a registered user
+function registeredSuccessifully(){
+    document.getElementById('login-page').style.display = 'none';
+    document.getElementById('chat-page').style.display = 'block';
+    document.getElementById('register-page').style.display = 'none';
+}
+
+//showing the settings fo a smaller screen and hiding it when ignored
+function showSettings() {
+  let settingsMenu =  document.getElementById('settings-menu');
+  if(settingsMenu.style.display=== "none"){
+    settingsMenu.style.display= "block";
+   // document.addEventListener("click", hideDiv);
+
+}else {
+    settingsMenu.style.display = "none";
+}
+/** adding an on ignore functionality*/
+}
+
+
+//changing the theme
+var light = true;
+function changeTheme(){
+    let theme_image = document.getElementById('theme-image');
+    let changeStyleSheet = document.getElementById("stylesheet");
+    if(light){
+        theme_image.src = '/Files/light-img.svg';
+        changeStyleSheet.href = "style.css";
+        light = false;
+    }else {
+        theme_image.src = '/Files/dark-img.svg';
+         changeStyleSheet.href = "dark.css"; 
+         light = true;
+    }
+}

f998bea7c30192d93df8975a2c53365a8c115a7e
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-19 10:00:39 +0300

    Validating user input v2

--- Layouts/script.js
+++ Layouts/script.js
@@ -51,12 +51,19 @@ function changeTheme(){
     }
 }
 
+//uses an event listener to prevent submiting the form is any of the inputs is invalid
 //function to validate the user inputs
-function validateForm() {
-    // Get form inputs
+    
+    const form = document.querySelector('#myForm');
+    const submitButton = document.querySelector('#submitButton');
+    submitButton.addEventListener('click', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+// Get form inputs
     const name = document.getElementById("user-name").value;
     const phone = document.getElementById("phone-no").value;
     const password = document.getElementById("password").value;
+//create variables to store the errors
     var nameError = document.getElementById('name-error-message');
     var phoneError = document.getElementById('phone-error-message');
     var passwordError = document.getElementById('password-error-message');
@@ -64,40 +71,59 @@ function validateForm() {
     const namePattern = /^[a-zA-Z ]+$/;
     const phonePattern = /^\d+$/;
     const passwordPattern = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{8,}$/;
+  
     //check that the fields are not empty
-    if (name.trim() === "") {
-        nameError.innerHTML =  "Please enter your name.";
-            return false;
-        }
-        if (phone.trim() === "") {
-        phoneError.innerHTML =  "Please enter your phone Number.";
-            return false;
-        }
-        if (password.trim() === "") {
-        passwordError.innerHTML = "Please enter your password.";
-            return false;
-        }
-    // Validate name
-    if (!name.match(namePattern)) {
-        nameError.innerHTML = "Name should contain only letters and spaces.";
+    if (name.trim() === "" && phone.trim() === "" && password.trim() === "") {
+        alert("Please fill in all fields.");
         return false;
-    }
-
-    // Validate phone number
-    if (!phone.match(phonePattern)) {
-       phoneError.innerHTML = "Phone number should contain only numbers.";
-        return false;
-    }
-
-    // Validate password
-    if (!password.match(passwordPattern)) {
-        passwordError.innerHTML ="Password should contain at least one letter, one number, and one special character and be at least 8 characters long.";
-        return false;
-    }
-
-    // If all inputs are valid, return true
-    nameError.innerHTML = "";
-    phoneError.innerHTML = "";
-    passwordError.innerHTML = "";
-    return true;
-}
+        }else {
+        //validating the user name
+            //Check the name if the name field is empty
+            if (name.trim() === "") {
+                nameError.innerHTML =  "Please enter your name.";
+                document.getElementById('user-name').style.borderColor = "red";
+                return false;
+                //else check that the format of the name is valid
+                }else if (!name.match(namePattern)) {
+                    nameError.innerHTML = "Name should contain only letters and spaces.";
+                    document.getElementById('user-name').style.borderColor = "red";
+                    return false;
+                    }else{
+                        //when everything is okay continue to the next step
+                        document.getElementById('user-name').style.borderColor = "green";
+                        nameError.innerHTML =  "";
+                         }
+        //validating the phone number
+            if (phone.trim() === "") {
+                phoneError.innerHTML =  "Please enter your phone Number.";
+                document.getElementById('phone-no').style.borderColor = "red";
+                return false;
+                }else  if (!phone.match(phonePattern)) {
+                    phoneError.innerHTML = "Phone number should contain only numbers.";
+                    document.getElementById('phone-no').style.borderColor = "red";
+                    return false;
+                    }else{
+                        phoneError.innerHTML =  "";
+                        document.getElementById('phone-no').style.borderColor = "green";
+                         }
+        //validating the user password
+            if (password.trim() === "") {
+                passwordError.innerHTML = "Please enter your password.";
+                document.getElementById('password').style.borderColor = "red";
+                return false;
+                }else  if (!password.match(passwordPattern)) {
+                    document.getElementById('password').style.borderColor = "red";
+                    passwordError.innerHTML ="Password should contain at least one letter, one number, and one special character and be at least 8 characters long.";
+                    return false;
+                    }else{
+                        passwordError.innerHTML =  "";
+                        document.getElementById('password').style.borderColor = "green";
+                        }
+                  }
+        // If all inputs are valid, return true
+            nameError.innerHTML = "";
+            phoneError.innerHTML = "";
+            passwordError.innerHTML = "";
+            
+            form.submit();
+});

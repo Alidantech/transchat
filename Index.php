1b00780d494918b1de6a98409e1b207b526eb38f
PETER IRUNGU <peteralidante254@gmail.com>
2023-09-13 14:25:48 +0300

    add new templates

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="public/assets/favicon.svg" type="image/x-icon">
+    <title>Welcome to wechat!</title>
+</head>
+<body>
+    <?php 
+        require "server/app.php";
+    ?>
+</body>
+</html>
---
+++ server/app.php
@@ -0,0 +1,35 @@
+<?php
+# Read the main.html file
+$main_template = __DIR__ . '/templates/main.html';
+$main_templateContent = file_get_contents($main_template);
+
+# Check if a page parameter is set, default to 'home' if not provided
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+# Define an array to map pages to their respective template files
+$page_templates = [
+    'home' => 'home.html',
+    'login' => 'login.html',
+    'signup' => 'signup.html',
+    'chat' => 'chat.html',
+];
+
+# Check if the requested page exists in the mapping
+if (array_key_exists($page, $page_templates)) {
+    # Load the content of the requested template
+    $template_file = __DIR__ . '/templates/' . $page_templates[$page];
+    if (file_exists($template_file)) {
+        $dynamicContent = file_get_contents($template_file);
+    } else {
+        $dynamicContent = "Template not found.";
+    }
+} else {
+    $dynamicContent = "Page not found.";
+}
+
+// Replace the {dynamic_content} placeholder with the dynamic content
+$main_templateContent = str_replace('{dynamic_content}', $dynamicContent, $main_templateContent);
+
+// Output the modified template
+echo $main_templateContent;
+?>

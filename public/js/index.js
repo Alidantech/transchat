ae8fa3ae9fce398706bab152880eac8b84dfb058
PETER IRUNGU <peteralidante254@gmail.com>
2023-10-05 17:59:58 +0300

    Restructure UI

--- public/js/index.js
+++ public/js/index.js
@@ -23,3 +23,36 @@ if (headers.length > 0) {
    // Attach the handleScroll function to the scroll event
    window.addEventListener("scroll", handleScroll);
 }
+
+
+// // Add an effect to display the menu for small devices
+//       const menuBtn = document.getElementById("menu-btn");
+//       const navBar = document.getElementById("nav-bar");
+
+//       menuBtn.addEventListener("click", function(){
+//             if (navBar.style.display === "none") {
+//                   navBar.style.display = "flex";
+//                   navBar.style.animation = "navBarAnimate 1s 1";
+//                   navBar.className = "toggled-nav ctr-column";
+                  
+//             } else {
+//                   navBar.className = "";
+//                   navBar.style.animation = "navBarRm 1s 1";
+//                   navBar.style.display = "none";
+//             }
+            
+//       });
+
+
+// toggle the side bar in chat page
+
+  const sideMenuBtn = document.getElementById("side-menu-btn");
+      const sideBar = document.getElementById("side-bar");
+
+      sideMenuBtn.addEventListener("click", function(){
+            if (sideBar.style.display === "none") {
+                  sideBar.style.display = "grid";
+            } else {
+                  sideBar.style.display = "none";
+            }
+      });
